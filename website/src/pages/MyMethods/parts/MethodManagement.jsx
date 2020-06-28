f7d802f98eb4f0f2c8a64f86010c4cdc881ebd0d
GraineFutee <pierre.asdrubal@gmail.com>
2020-06-28 19:08:35 +0200

    Mymethod second part

--- website/src/pages/MyMethods/parts/MethodManagement.jsx
+++ website/src/pages/MyMethods/parts/MethodManagement.jsx
@@ -8,6 +8,11 @@ export default function MethodManagement(props) {
     "Team3",
     "Team4",
     "Team5",
+    "Team6",
+    "Team7",
+    "Team8",
+    "Team9",
+    "Team10",
   ]);
   const [modal, setModal] = useState({ active: false, for: "", value: [] });
   return (
@@ -30,7 +35,7 @@ export default function MethodManagement(props) {
             <div className="field is-grouped is-grouped-multiline">
               <div className="field has-addons control">
                 <div className="control">
-                  <span className="select">
+                  <span className="select is-small">
                     <select>
                       <option>$</option>
                       <option>£</option>
@@ -41,13 +46,13 @@ export default function MethodManagement(props) {
                 <div className="control">
                   <input
                     type="number"
-                    className="input"
+                    className="input is-small"
                     placeholder="Bet ..."
                   />
                 </div>
               </div>
               <div className="control">
-                <span className="select">
+                <span className="select is-small">
                   <select>
                     <option>On ...</option>
                     <option>Draw</option>
@@ -59,12 +64,14 @@ export default function MethodManagement(props) {
               {method.onTeams ? (
                 <div className="field has-addons control">
                   <div className="control">
-                    <span className="select">
+                    <span className="select is-small">
                       <select
                         value={method.on2 ? method.on2 : "On ..."}
                         onChange={(event) => {
                           let newMethod = { ...method };
                           newMethod.on2 = event.currentTarget.value;
+                          if (event.currentTarget.value !== "Teams")
+                            newMethod.onTeams = null;
                           setMethod(newMethod);
                         }}
                       >
@@ -76,7 +83,7 @@ export default function MethodManagement(props) {
                   </div>
                   <div className="control" id="team1">
                     <a
-                      className="button is-link"
+                      className="button is-link is-small"
                       onClick={() => {
                         setModal({
                           active: true,
@@ -89,7 +96,7 @@ export default function MethodManagement(props) {
                 </div>
               ) : (
                 <div className="control">
-                  <span className="select">
+                  <span className="select is-small">
                     <select
                       value={method.on2 ? method.on2 : "On ..."}
                       onChange={(event) => {
@@ -113,6 +120,140 @@ export default function MethodManagement(props) {
                   </span>
                 </div>
               )}
+              <div className="control">
+                <span className="select is-small">
+                  <select>
+                    <option>Playing ...</option>
+                    <option>Home</option>
+                    <option>Away</option>
+                    <option>Any</option>
+                  </select>
+                </span>
+              </div>
+              {method.againstTeams ? (
+                <div className="field has-addons control">
+                  <div className="control">
+                    <span className="select is-small">
+                      <select
+                        value={method.against ? method.against : "Against ..."}
+                        onChange={(event) => {
+                          let newMethod = { ...method };
+                          newMethod.against = event.currentTarget.value;
+                          if (event.currentTarget.value !== "Teams")
+                            newMethod.againstTeams = null;
+                          setMethod(newMethod);
+                        }}
+                      >
+                        <option>Against ...</option>
+                        <option>Teams</option>
+                        <option>Any</option>
+                      </select>
+                    </span>
+                  </div>
+                  <div className="control" id="team1">
+                    <a
+                      className="button is-link is-small"
+                      onClick={() => {
+                        setModal({
+                          active: true,
+                          for: "against",
+                          value: [...method.againstTeams],
+                        });
+                      }}
+                    >{`${method.againstTeams.length} T`}</a>
+                  </div>
+                </div>
+              ) : (
+                <div className="control">
+                  <span className="select is-small">
+                    <select
+                      value={method.against ? method.against : "Against ..."}
+                      onChange={(event) => {
+                        let newMethod = { ...method };
+                        if (event.currentTarget.value === "Teams") {
+                          newMethod.againstTeams = [];
+                          setModal({
+                            active: true,
+                            for: "against",
+                            value: [],
+                          });
+                        }
+                        newMethod.against = event.currentTarget.value;
+                        setMethod(newMethod);
+                      }}
+                    >
+                      <option>Against ...</option>
+                      <option>Teams</option>
+                      <option>Any</option>
+                    </select>
+                  </span>
+                </div>
+              )}
+            </div>
+            <div className="box">
+              <h3 className="title is-5">Conditions</h3>
+              {method.conditions &&
+                method.conditions.map((condition) => (
+                  <div
+                    className="field is-grouped is-grouped-multiline"
+                    key={condition.id}
+                  >
+                    <div className="control">
+                      <span className="select is-small">
+                        <select>
+                          <option>On ...</option>
+                          <option>The Team</option>
+                          <option>The Odd</option>
+                        </select>
+                      </span>
+                    </div>
+                    <div className="control">
+                      <a
+                        className="button is-danger is-small"
+                        id={condition.id}
+                        onClick={(event) => {
+                          let newMethod = { ...method };
+                          newMethod.conditions.splice(
+                            newMethod.conditions.findIndex(
+                              (condition) =>
+                                condition.id == event.currentTarget.id
+                            ),
+                            1
+                          );
+                          setMethod(newMethod);
+                        }}
+                      >
+                        <i className="fas fa-trash-alt"></i>
+                      </a>
+                    </div>
+                  </div>
+                ))}
+              <a
+                className="button is-light is-small"
+                onClick={() => {
+                  let newMethod = { ...method };
+                  if (!newMethod.conditions) {
+                    newMethod.conditions = [];
+                  }
+                  newMethod.conditions.push({
+                    id:
+                      newMethod.conditions.length > 0
+                        ? newMethod.conditions[newMethod.conditions.length - 1]
+                            .id + 1
+                        : 0,
+                    on: "",
+                    value1: "",
+                    value2: "",
+                  });
+                  setMethod(newMethod);
+                }}
+              >
+                <i className="fas fa-plus-circle"></i>
+              </a>
+            </div>
+            <div className="buttons">
+              <button className="button is-success">Save</button>
+              <button className="button is-danger">Cancel</button>
             </div>
           </form>
         </div>
@@ -122,11 +263,15 @@ export default function MethodManagement(props) {
         <div className="modal-content">
           <div className="box">
             <h3 className="title is-4">Choose Teams</h3>
-            {modal.for === "on" ? (
-              <div className="columns is-multiline box">
+            {modal.active && (
+              <div className="columns is-multiline box is-mobile">
                 {teams.map((team) => (
                   <div className="column">
-                    <label className="checkbox" key={team}>
+                    <label
+                      className="checkbox"
+                      key={team}
+                      style={{ whiteSpace: "nowrap" }}
+                    >
                       <input
                         id={team}
                         type="checkbox"
@@ -143,23 +288,28 @@ export default function MethodManagement(props) {
                           }
                           setModal(newModal);
                         }}
-                      />
-
+                      />{" "}
                       {team}
                     </label>
                   </div>
                 ))}
               </div>
-            ) : (
-              <></>
             )}
             <div className="buttons">
               <a
-                className="button is-success"
+                className="button is-success is-small"
                 onClick={() => {
                   let newMethod = { ...method };
-                  if (modal.for === "on") {
-                    newMethod.onTeams = modal.value;
+                  switch (modal.for) {
+                    case "on":
+                      newMethod.onTeams = modal.value;
+                      break;
+                    case "against":
+                      newMethod.againstTeams = modal.value;
+                      break;
+
+                    default:
+                      break;
                   }
                   setMethod(newMethod);
                   setModal({ active: false, for: "", value: [] });
@@ -169,13 +319,28 @@ export default function MethodManagement(props) {
                 Save
               </a>
               <a
-                className="button is-danger"
+                className="button is-danger is-small"
                 onClick={() => {
-                  if (modal.for === "on" && method.onTeams.length === 0) {
-                    let newMethod = { ...method };
-                    newMethod.onTeams = null;
-                    newMethod.on2 = null;
-                    setMethod(newMethod);
+                  switch (modal.for) {
+                    case "on":
+                      if (method.onTeams.length === 0) {
+                        let newMethod = { ...method };
+                        newMethod.onTeams = null;
+                        newMethod.on2 = null;
+                        setMethod(newMethod);
+                      }
+                      break;
+                    case "against":
+                      if (method.againstTeams.length === 0) {
+                        let newMethod = { ...method };
+                        newMethod.againstTeams = null;
+                        newMethod.against = null;
+                        setMethod(newMethod);
+                      }
+                      break;
+
+                    default:
+                      break;
                   }
                   setModal({ active: false, for: "", value: [] });
                 }}
